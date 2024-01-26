5a25badbfdd8cd32682b3918e02ed10f1577ba2d
Arslan Rana <rajputarslan693@gmail.com>
2024-01-27 03:10:04 +0500

    started another project for practice explored yarn pkg in code created header component

---
+++ src/app/components/layout/header.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header(){
+    return(
+        <header className="flex items-center justify-between">
+        <Link className="text-primary font-semibold text text-2xl" href="">ST PIZZA</Link>
+        <nav className="flex gap-8 items-center text-gray-500 font-semibold">
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Contact</Link>
+          <Link className="bg-primary rounded-full text-white px-8 py-2" href={''}>Login</Link>
+        </nav>
+      </header>
+    );
+}
